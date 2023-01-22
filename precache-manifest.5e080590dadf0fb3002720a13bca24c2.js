960dada54b1a48d56e03846d353d01b2ac98dee4
Henryck <ryck302@gmail.com>
2023-01-21 21:45:03 -0300

    Updates

---
+++ precache-manifest.5e080590dadf0fb3002720a13bca24c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2817f1f8ed577b1d71cb2f66be0ebcab",
+    "url": "/trivia-react/index.html"
+  },
+  {
+    "revision": "cc45da910f222fce20b1",
+    "url": "/trivia-react/static/css/main.84d2d491.chunk.css"
+  },
+  {
+    "revision": "12ba03240dc23eb16f03",
+    "url": "/trivia-react/static/js/2.74c87d09.chunk.js"
+  },
+  {
+    "revision": "ca6b5a6a129747daa0202cfb1dcfa4c4",
+    "url": "/trivia-react/static/js/2.74c87d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc45da910f222fce20b1",
+    "url": "/trivia-react/static/js/main.93c56b19.chunk.js"
+  },
+  {
+    "revision": "18074418e86b82ce8d8d",
+    "url": "/trivia-react/static/js/runtime-main.60ffb1dd.js"
+  }
+]);
